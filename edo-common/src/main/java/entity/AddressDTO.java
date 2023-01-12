824ba322e7ec14213951a43f0d88d47e8425b376
Cilez <kobrelislegryi@gmail.com>
2023-01-12 19:44:58 +0300

    #30 create AddressDTO

---
+++ edo-common/src/main/java/entity/AddressDTO.java
@@ -0,0 +1,61 @@
+package entity;
+
+/**
+ * Представляет DTO сущности Address.
+ *
+ * @author Сергей Иваненко
+ * @version 1.0
+ * @since 1.0
+ */
+public class AddressDTO {
+
+    /**
+     * Адрес, полностью.
+     */
+    private String fullAddress;
+
+    /**
+     * Улица
+     */
+    private String street;
+
+    /**
+     * Номер дома
+     */
+    private String house;
+
+    /**
+     * Индекс
+     */
+    private String index;
+
+    /**
+     * Корпус
+     */
+    private String housing;
+
+    /**
+     * Строение
+     */
+    private String building;
+
+    /**
+     * Город
+     */
+    private String city;
+
+    /**
+     * Регион
+     */
+    private String region;
+
+    /**
+     * Страна
+     */
+    private String country;
+
+    /**
+     * Номер квартиры, или этаж
+     */
+    private String flat;
+}
