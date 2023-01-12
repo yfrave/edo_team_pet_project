06ff5a09d5ba22af99107d15810742049fcd8eae
Антон Латышев <antohahaha91@gmail.com>
2023-01-13 02:32:17 +0300

    Create entity Question

---
+++ edo-repository/src/main/java/com/education/entity/Question.java
@@ -0,0 +1,39 @@
+package com.education.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.sql.Date;
+
+/**
+ * Класс Question.
+ * Хранит краткое содержание обращений.
+ */
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "question")
+public class Question extends BaseEntity{
+    /**
+     * Дата создания обращения.
+     */
+    @Column(name = "creation_date", nullable = false)
+    private Date creationDate;
+
+    /**
+     * Дата отклика на обращение.
+     */
+    @Column(name = "archived_date", nullable = false)
+    private Date archivedDate;
+
+    /**
+     * Краткое содержание обращения.
+     */
+    @Column(nullable = false)
+    private String summary;
+}
