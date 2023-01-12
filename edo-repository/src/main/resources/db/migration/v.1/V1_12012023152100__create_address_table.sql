8edb829d877238569a65f4b96f108982dc3f589d
Cilez <kobrelislegryi@gmail.com>
2023-01-12 19:26:26 +0300

    added comments for columns

--- edo-repository/src/main/resources/db/migration/v.1/V1_12012023152100__create_address_table.sql
+++ edo-repository/src/main/resources/db/migration/v.1/V1_12012023152100__create_address_table.sql
@@ -1,4 +1,4 @@
-create table if not exists edo.address(
+create table if not exists address(
     id bigserial not null primary key,
     full_address text,
     street text,
@@ -11,3 +11,16 @@ create table if not exists edo.address(
     country text,
     flat text
 );
+
+comment on column address.id is 'Это id, чего ещё ты ожидаешь здесь увидеть?';
+comment on column address.full_address is 'Полный адрес';
+comment on column address.street is 'Улица';
+comment on column address.house is 'Номер дома';
+comment on column address.index is 'Индекс';
+comment on column address.housing is 'Корпус';
+comment on column address.building is 'Строение';
+comment on column address.city is 'Город';
+comment on column address.region is 'Регион';
+comment on column address.country is 'Странна';
+comment on column address.flat is 'Этаж или номер квартиры';
+
