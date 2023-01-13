ae6731fc68fdb28a8b060666c1f2b19361c9d321
Cilez <kobrelislegryi@gmail.com>
2023-01-13 13:21:36 +0300

    #30 added @NotNull constraints in Address.java

--- edo-repository/src/main/java/com/education/entity/Address.java
+++ edo-repository/src/main/java/com/education/entity/Address.java
@@ -34,40 +34,48 @@ public class Address extends BaseEntity {
     /**
      * Номер дома
      */
+    @NotNull(message = "house cannot be null")
     private String house;
 
     /**
      * Индекс
      */
+    @NotNull(message = "index cannot be null")
     private String index;
 
     /**
      * Корпус
      */
+    @NotNull(message = "housing cannot be null")
     private String housing;
 
     /**
      * Строение
      */
+    @NotNull(message = "building cannot be null")
     private String building;
 
     /**
      * Город
      */
+    @NotNull(message = "city cannot be null")
     private String city;
 
     /**
      * Регион
      */
+    @NotNull(message = "region cannot be null")
     private String region;
 
     /**
      * Страна
      */
+    @NotNull(message = "country cannot be null")
     private String country;
 
     /**
      * Номер квартиры, или этаж
      */
+    @NotNull(message = "flat cannot be null")
     private String flat;
 }
