3b6b3fc414678c163d89e2a7be3f533d5b8946a4
dmitriidevcrew <dmitrii.pavlovich.dev@bk.ru>
2023-01-13 07:20:45 +0300

    .

--- edo-repository/src/main/resources/db/migration/v.1/v_1_1201202360411_create_resolution_table.sql
+++ edo-repository/src/main/resources/db/migration/v.1/v_1_1201202360411_create_resolution_table.sql
@@ -1,21 +1,23 @@
 create table if not exists resolution
 (
     id               bigserial not null primary key,
-    creation_date    timestamp with time zone,
-    archived_date    timestamp with time zone,
-    last_action_date timestamp with time zone,
-    resolution_name  varchar,
-    creator_id       bigint,
-    signer_id        bigint,
-    curator_id       bigint
-); --Таблица описывающая резолюцию
+    creation_date    date,
+    archived_date    date,
+    last_action_date date,
+    resolution_name  varchar(30),
+    creator          bigserial,
+    signer           bigserial,
+    executor         bigserial,
+    curator          bigserial
+);
 
 comment on column resolution.id is 'id';
 comment on column resolution.creation_date is 'Дата создания резолюции';
-comment on column resolution.archived_date is 'Дата архивации';
+comment on column resolution.archived_date  is 'Дата архивации';
 comment on column resolution.last_action_date is 'Дата последнего события';
 comment on column resolution.resolution_name is 'Переменная отражающая тип решения';
-comment on column resolution.creator_id is 'id Работника создавший резолюцию';
-comment on column resolution.signer_id is 'id Работника подписывающий документ';
-comment on column resolution.curator_id is 'id Работника курирующий работу';
+comment on column resolution.creator is 'id Работника создавший резолюцию';
+comment on column resolution.signer is 'id Работника подписывающий документ';
+comment on column resolution.executor is 'id Работников выполняющие решение';
+comment on column resolution.curator is 'id Работника курирующий работу';
 
