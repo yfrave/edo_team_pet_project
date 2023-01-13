4528a5b7d6a4c48fdec88d3b21f67e30272318ed
dmitriidevcrew <dmitrii.pavlovich.dev@bk.ru>
2023-01-13 07:20:45 +0300

    Create Resolution, ResolutionDTO POJO class, database migration script

---
+++ edo-common/src/main/java/entity/ResolutionDTO.java
@@ -0,0 +1,61 @@
+package entity;
+
+import java.sql.Date;
+import java.util.List;
+
+/**
+ * POJO класс, содержащий информацию о датах и работниках.
+ * @author Dmitrii Ermolenko
+ */
+public class ResolutionDTO extends BaseEntityDTO {
+
+    /**
+     * Возможные типы решения
+     */
+    private enum resolutionEnum{
+        Резолюция,
+        направление,
+        запрос
+    }
+
+    /**
+     * Дата создания резолюции
+     */
+    private Date creationDate;
+
+    /**
+     * Дата архивации
+     */
+    private Date archivedDate;
+
+    /**
+     * Дата последнего события
+     */
+    private Date lastActionDate;
+
+    /**
+     * Переменная отражающая тип решения
+     */
+
+    private resolutionEnum resolutionEnum;
+
+    /**
+     * Работник создавший резолюцию
+     */
+    private EmployeeDTO creator;
+
+    /**
+     * Работник подписывающий документ
+     */
+    private EmployeeDTO signer;
+
+    /**
+     * Работники выполняющие решение
+     */
+    private List<EmployeeDTO> executor;
+
+    /**
+     * Работник курирующий работу
+     */
+    private EmployeeDTO curator;
+}
