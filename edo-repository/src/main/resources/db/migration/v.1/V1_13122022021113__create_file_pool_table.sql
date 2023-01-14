edd7386e983b4a9972b1fdcaac8ffe1ee4fd0bfc
Ramazan99 <mm.gadzhiev@mail.ru>
2023-01-15 00:44:33 +0300

    #25 added comments in sql migration

--- edo-repository/src/main/resources/db/migration/v.1/V1_13122022021113__create_file_pool_table.sql
+++ edo-repository/src/main/resources/db/migration/v.1/V1_13122022021113__create_file_pool_table.sql
@@ -1,13 +1,13 @@
 CREATE TABLE if not exists file_pool
-(   id bigserial not null primary key,
-    storage_file_id UUID not null,
-    file_name varchar(255),
-    extension varchar(255),
-    file_size int,
-    page_count int,
-    upload_date timestamp with timezone,
-    archived_date timestamp with timezone,
-    creator_id bigint
+(   id bigserial not null primary key,      --ID
+    storage_file_id UUID not null,          --ID хранилища файла
+    file_name varchar(255),                 --Имя хранилища файла
+    extension varchar(255),                 --Расширение файла
+    file_size int,                          --Размер файла
+    page_count int,                         --Количество страниц файла
+    upload_date timestamp with timezone,    --Дата загрузки файла
+    archived_date timestamp with timezone,  --Дата архивирования файла
+    creator_id bigint                       --Автор файла
 );
 comment on column file_pool.id is 'ID'
 comment on column file_pool.storage_file_id is 'ID хранилища файла';
