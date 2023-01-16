4c84cc57eeb727228e9a2ab0274145cd4becbaeb
Сергей_Иваненко <kobrelislegryi@gmail.com>
2023-01-16 13:59:15 +0300

    36 added @Transactional in AddressServiceImpl.java

--- edo-repository/src/main/java/com/education/service/address/impl/AddressServiceImpl.java
+++ edo-repository/src/main/java/com/education/service/address/impl/AddressServiceImpl.java
@@ -4,29 +4,35 @@ import com.education.entity.Address;
 import com.education.repository.AddressRepository;
 import com.education.service.address.AddressService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+
 @Service
 public class AddressServiceImpl implements AddressService {
     private AddressRepository addressRepository;
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void save(Address obj) {
         addressRepository.save(obj);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void delete(Address obj) {
         addressRepository.delete(obj);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class, readOnly = true)
     public Optional<Address> findById(Long id) {
         return addressRepository.findById(id);
     }
 
     @Override
+    @Transactional(rollbackFor = Exception.class, readOnly = true)
     public List<Address> findAllById(Iterable<Long> list) {
         return addressRepository.findAllById(list);
     }
