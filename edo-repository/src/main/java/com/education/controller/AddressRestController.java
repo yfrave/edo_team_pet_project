28d65ce4229d645e25e919ff775f198a9d06998f
Сергей_Иваненко <kobrelislegryi@gmail.com>
2023-01-16 15:32:58 +0300

    36 added swagger annotations to AddressRestController

--- edo-repository/src/main/java/com/education/controller/AddressRestController.java
+++ edo-repository/src/main/java/com/education/controller/AddressRestController.java
@@ -18,14 +18,14 @@ import java.util.Optional;
 public class AddressRestController {
     private AddressService addressService;
 
-    @ApiOperation(value = "Get an address by id", notes = "Returns an address as per the id")
+    @ApiOperation(value = "Получить адрес по d", notes = "Returns an address as per the id")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Successfully retrieved"),
             @ApiResponse(code = 404, message = "Not found - The address was not found")
     })
     @GetMapping("/{id}")
     public ResponseEntity<Address> findAddress(@PathVariable("id")
-                                               @ApiParam(name = "id", value = "Product id", example = "1")
+                                               @ApiParam(name = "id", value = "Адрес id", example = "1")
                                                Long id) {
         Optional<Address> address = addressService.findById(id);
         return address.isEmpty()
@@ -33,22 +33,34 @@ public class AddressRestController {
                 : new ResponseEntity<>(address.get(), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Получить список адресов", notes = "Находит адреса по их id. Возвращает списком List<Address>")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved"),
+            @ApiResponse(code = 404, message = "Not found - The addresses was not found")
+    })
     @GetMapping("/findAll")
-    public ResponseEntity<List<Address>> findAllAddresses(List<Long> idList) { // Доделать приём листа
+    public ResponseEntity<List<Address>> findAllAddresses(@RequestParam
+                                                          @ApiParam(name = "Address list")
+                                                          List<Long> idList) {
         List<Address> list = addressService.findAllById(idList);
         return list != null && !list.isEmpty()
                 ? new ResponseEntity<>(list, HttpStatus.OK)
                 : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @ApiOperation(value = "Сохранить адрес")
+    @ApiResponse(code = 200, message = "Successfully created")
     @PostMapping("/")
-    public ResponseEntity<Address> save(@RequestBody Address address) {
+    public ResponseEntity<Address> save(@RequestBody @ApiParam("Address") Address address) {
         addressService.save(address);
         return new ResponseEntity<>(address, HttpStatus.CREATED);
     }
 
+    @ApiOperation(value = "Удалить адрес")
+    @ApiResponse(code = 200, message = "Successfully deleted")
+    @PostMapping("/")
     @DeleteMapping("/delete")
-    public ResponseEntity<Void> deleteUser(@RequestBody Address address) {
+    public ResponseEntity<Void> deleteUser(@RequestBody @ApiParam("Address") Address address) {
         addressService.delete(address);
         return ResponseEntity.ok().build();
     }
