79c9c4a053ee00a375a0faa4dc46302e83d52ef7
Сергей_Иваненко <kobrelislegryi@gmail.com>
2023-01-17 14:34:52 +0300

    36 mmmmmdaaaa

--- edo-repository/src/main/java/com/education/repository/AddressRepository.java
+++ edo-repository/src/main/java/com/education/repository/AddressRepository.java
@@ -2,6 +2,8 @@ package com.education.repository;
 
 import com.education.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
