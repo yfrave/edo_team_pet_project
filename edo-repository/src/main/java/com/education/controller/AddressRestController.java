bc7b30380c25bdb8a1b0dae1fe436c371cbcf72e
Сергей_Иваненко <kobrelislegryi@gmail.com>
2023-01-19 17:10:02 +0300

    36 tested the controller

--- edo-repository/src/main/java/com/education/controller/AddressRestController.java
+++ edo-repository/src/main/java/com/education/controller/AddressRestController.java
@@ -8,9 +8,11 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,7 +28,7 @@ public class AddressRestController {
             @ApiResponse(code = 404, message = "Not found - The address was not found")
     })
     @GetMapping("/{id}")
-    public ResponseEntity<Address> findAddress(@PathVariable("id")
+    public ResponseEntity<Address> fetchAddress(@PathVariable("id")
                                                @ApiParam(name = "id", value = "Адрес id", example = "1")
                                                Long id) {
         Optional<Address> address = addressService.findById(id);
@@ -35,13 +37,22 @@ public class AddressRestController {
                 : new ResponseEntity<>(address.get(), HttpStatus.OK);
     }
 
+//    @GetMapping("/test")
+//    public ResponseEntity<List<Long>> test() {
+//        List<Long> list = new ArrayList<>();
+//        list.add(1l);
+//        list.add(2l);
+//        list.add(3l);
+//        return new ResponseEntity<>(list, HttpStatus.OK);
+//    }
+
     @ApiOperation(value = "Получить список адресов", notes = "Находит адреса по их id. Возвращает списком List<Address>")
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Successfully retrieved"),
             @ApiResponse(code = 404, message = "Not found - The addresses was not found")
     })
     @GetMapping("/findAll")
-    public ResponseEntity<List<Address>> findAllAddresses(@RequestParam
+    public ResponseEntity<List<Address>> fetchAllAddresses(@RequestBody
                                                           @ApiParam(name = "Address list")
                                                           List<Long> idList) {
         List<Address> list = addressService.findAllById(idList);
