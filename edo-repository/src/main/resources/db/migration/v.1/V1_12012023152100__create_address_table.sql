2aea35d3b1666e66c5e71b1f7db6c24f87d948e9
Alik Karibov <alik_karibov@mail.ru>
2023-01-20 18:13:39 +0300

    fixed migrations

--- edo-repository/src/main/resources/db/migration/v.1/V1_12012023152100__create_address_table.sql
+++ edo-repository/src/main/resources/db/migration/v.1/V1_12012023152100__create_address_table.sql
@@ -1,27 +1,31 @@
-create table if not exists address
+create table if not exists resolution
 (
-    id           bigserial not null primary key, --id
-    full_address text,                           --Полный адрес
-    street       text,                           --Улица
-    house        text,                           --Номер дома
-    index        text,                           --Индекс
-    housing      text,                           --Корпус
-    building     text,                           --Строение
-    city         text,                           --Город
-    region       text,                           --Регион
-    country      text,                           --Страна
-    flat         text                            --Этаж или номер квартиры
-);
+    id               bigserial not null primary key,
+    creation_date    timestamp with time zone,
+    archived_date    timestamp with time zone,
+    last_action_date timestamp with time zone,
+    resolution_name  varchar,
+    creator_id bigint not null unique references employee (id),
+    signer_id bigint not null unique references employee (id),
+    curator_id bigint not null unique references employee (id)
+    ); --Таблица описывающая резолюцию
 
-comment on column address.id is 'Это id, чего ещё ты ожидаешь здесь увидеть?';
-comment on column address.full_address is 'Полный адрес';
-comment on column address.street is 'Улица';
-comment on column address.house is 'Номер дома';
-comment on column address.index is 'Индекс';
-comment on column address.housing is 'Корпус';
-comment on column address.building is 'Строение';
-comment on column address.city is 'Город';
-comment on column address.region is 'Регион';
-comment on column address.country is 'Странна';
-comment on column address.flat is 'Этаж или номер квартиры';
+create table if not exists resolutions_employees
+(
+    resolution_id bigint references resolution (id),
+    employee_id bigint references employee (id)
+    ); --Таблица one to many
+
+comment on column resolutions_employees.resolution_id is 'id resolution';
+comment on column resolutions_employees.employee_id is 'id employee';
+
+comment on column resolution.id is 'id';
+comment on column resolution.creation_date is 'Дата создания резолюции';
+comment on column resolution.archived_date is 'Дата архивации';
+comment on column resolution.last_action_date is 'Дата последнего события';
+comment on column resolution.resolution_name is 'Переменная отражающая тип решения';
+comment on column resolution.creator_id is 'id Работника создавший резолюцию';
+comment on column resolution.signer_id is 'id Работника подписывающий документ';
+comment on column resolution.curator_id is 'id Работника курирующий работу';
+
 
