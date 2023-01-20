f0ad43af2570bea2aa86f074ea4c9f36bb00af73
Alik Karibov <alik_karibov@mail.ru>
2023-01-20 18:05:29 +0300

    fixed migrations

---
+++ edo-repository/src/main/resources/db/migration/v.1/V1_13012023024443__create_question_table.sql
@@ -0,0 +1,13 @@
+create table if not exists question
+(
+    id bigserial not null primary key,          -- id
+    creation_date timestamptz not null,         -- дата создания обращения
+    archived_date timestamptz,                  -- дата архивирования обращения
+    summary varchar(255) not null               -- краткое содержание обращения
+    );
+
+comment on table question is 'хранит краткое содержание обращений';
+comment on column question.id is 'id обращения';
+comment on column question.creation_date is 'дата создания обращения';
+comment on column question.archived_date is 'дата архивирования обращения';
+comment on column question.summary is 'краткое содержание обращения';
