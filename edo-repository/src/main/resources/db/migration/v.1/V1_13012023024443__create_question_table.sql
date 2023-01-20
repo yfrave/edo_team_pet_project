bc0a444d5fdb649fd96d1fb3f2efbe14d1efaa30
Alik Karibov <alik_karibov@mail.ru>
2023-01-20 18:18:35 +0300

    fixed migrations

--- edo-repository/src/main/resources/db/migration/v.1/V1_13012023024443__create_question_table.sql
+++ edo-repository/src/main/resources/db/migration/v.1/V1_13012023024443__create_question_table.sql
@@ -4,7 +4,8 @@ create table if not exists question
     creation_date timestamptz not null,         -- дата создания обращения
     archived_date timestamptz,                  -- дата архивирования обращения
     summary varchar(255) not null               -- краткое содержание обращения
-    );
+);
+
 comment on table question is 'хранит краткое содержание обращений';
 comment on column question.id is 'id обращения';
 comment on column question.creation_date is 'дата создания обращения';
