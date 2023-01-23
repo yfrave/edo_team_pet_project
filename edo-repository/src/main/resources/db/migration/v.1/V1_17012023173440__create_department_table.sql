ad44ec7db5177dce159d5660666613d39d26a05e
Сергей_Иваненко <kobrelislegryi@gmail.com>
2023-01-23 11:53:55 +0300

    36 returned comments to migration

--- edo-repository/src/main/resources/db/migration/v.1/V1_17012023173440__create_department_table.sql
+++ edo-repository/src/main/resources/db/migration/v.1/V1_17012023173440__create_department_table.sql
@@ -16,3 +16,13 @@ alter table if exists department
 
 alter table if exists department
     add constraint jk_uq1_department_department foreign key (department_id) references department;
+
+comment on column department.id is 'id';
+comment on column department.short_name is 'аббревиатура';
+comment on column department.full_name is 'Полное имя';
+comment on column department.address_id is 'ключ адреса';
+comment on column department.archived_date is 'дата закрытия';
+comment on column department.creation_date is 'дата основания';
+comment on column department.external_id is 'ключ внешней таблицы';
+comment on column department.phone is 'номер телефона';
+comment on column department.department_id is 'ключ вышестоящего отдела';
