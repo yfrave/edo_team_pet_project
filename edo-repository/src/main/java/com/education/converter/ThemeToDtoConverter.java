0786a94274ca9a4ad04ea3f14eb8469ac5297b6e
Alexey Sementkovskiy <sementkovsky@yandex.ru>
2023-01-25 17:04:48 +0300

    ThemeToDtoConverter created

---
+++ edo-repository/src/main/java/com/education/converter/ThemeToDtoConverter.java
@@ -0,0 +1,39 @@
+package com.education.converter;
+
+import com.education.entity.Theme;
+import com.education.model.dto.ThemeDto;
+import com.education.repository.theme.ThemeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ThemeToDtoConverter {
+
+    private final ThemeRepository themeRepository;
+
+    public ThemeDto convertThemeToDto(Theme theme) {
+        return new ThemeDto(theme.getId(),
+                theme.getName(),
+                theme.getCreationDate(),
+                theme.getArchivedDate(),
+                theme.getCode(),
+                theme.getParentTheme().getId());
+    }
+
+    /**
+     * Метод конвертирует ДТО в тему
+     */
+    public Theme convertDtoToTheme(ThemeDto themeDto) {
+
+        Theme parentTheme = themeDto.getIdParentTheme() == null
+                ? null
+                : themeRepository.findById(themeDto.getIdParentTheme()).orElse(null);
+
+        return new Theme(themeDto.getName(),
+                themeDto.getCreationDate(),
+                themeDto.getArchivedDate(),
+                themeDto.getCode(),
+                parentTheme);
+    }
+}
