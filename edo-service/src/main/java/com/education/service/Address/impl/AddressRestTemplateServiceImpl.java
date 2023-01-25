4347ed5f181ec3ac6715e78e20affd256c2a9103
Сергей_Иваненко <kobrelislegryi@gmail.com>
2023-01-25 13:43:42 +0300

    added javadoc annotations

--- edo-service/src/main/java/com/education/service/Address/impl/AddressRestTemplateServiceImpl.java
+++ edo-service/src/main/java/com/education/service/Address/impl/AddressRestTemplateServiceImpl.java
@@ -4,7 +4,6 @@ import com.education.model.dto.AddressDto;
 import com.education.service.Address.AddressService;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
-import com.netflix.discovery.shared.Application;
 import lombok.AllArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
@@ -13,15 +12,14 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
+/**
+ * Сервис для контроллера, работает с AddressDto
+ */
 @Service
 @AllArgsConstructor
 public class AddressRestTemplateServiceImpl implements AddressService {
-    private final String URL = "http://edo-repository/api/repository/address/";
 
     /**
      * Клиент для отправки и получения запросов
@@ -34,20 +32,23 @@ public class AddressRestTemplateServiceImpl implements AddressService {
     private final EurekaClient eurekaClient;
 
     /**
-     * путь до рест контроллера address
+     * путь до рест контроллера репозитория
      */
     private final String baseUrl = "/api/repository/address";
 
     /**
-     * Имя модуля в который отправляются запросы
+     * Имя модуля, в который отправляются запросы
      */
     private final String serviceName = "edo-repository";
 
 
     @Override
     public AddressDto getById(Long id) {
-        Application app = eurekaClient.getApplication(serviceName);
-        InstanceInfo instance = app.getInstances().stream().findAny().get();
+        InstanceInfo instance = eurekaClient.getApplication(serviceName)
+                .getInstances()
+                .stream()
+                .findAny()
+                .orElseThrow();
         URI uri = UriComponentsBuilder.fromPath(baseUrl + "/{id}")
                 .scheme("http")
                 .host(instance.getHostName())
@@ -55,30 +56,38 @@ public class AddressRestTemplateServiceImpl implements AddressService {
                 .buildAndExpand(id)
                 .toUri();
         var request = new RequestEntity<>(null, HttpMethod.GET, uri);
+
         return template.exchange(request, AddressDto.class).getBody();
     }
 
     @Override
     public List<AddressDto> fetchAddressedList(List<Long> idList) {
-        var app = eurekaClient.getApplication(serviceName);
-        var instance = app.getInstances().stream().findAny().get();
+        InstanceInfo instance = eurekaClient.getApplication(serviceName)
+                .getInstances()
+                .stream()
+                .findAny()
+                .orElseThrow();
         var uri = UriComponentsBuilder.fromPath(baseUrl + "/findAll")
                 .scheme("http")
                 .host(instance.getHostName())
                 .port(instance.getPort())
                 .build()
                 .toUri();
-        var request = new RequestEntity<>(idList, HttpMethod.GET, uri);
-        var myBean = new ParameterizedTypeReference<List<AddressDto>>() {};
-        var response = template.exchange(request, List.class);
-//        return response.getBody();
-        return new ArrayList<>();
+        var request = new RequestEntity<>(idList, HttpMethod.POST, uri);
+        var myBean = new ParameterizedTypeReference<List<AddressDto>>() {
+        };
+        var response = template.exchange(request, myBean);
+
+        return response.getBody();
     }
 
     @Override
     public AddressDto save(AddressDto address) {
-        var app = eurekaClient.getApplication(serviceName);
-        var instance = app.getInstances().stream().findAny().get();
+        InstanceInfo instance = eurekaClient.getApplication(serviceName)
+                .getInstances()
+                .stream()
+                .findAny()
+                .orElseThrow();
         var uri = UriComponentsBuilder.fromPath(baseUrl + "/")
                 .scheme("http")
                 .host(instance.getHostName())
@@ -86,20 +95,23 @@ public class AddressRestTemplateServiceImpl implements AddressService {
                 .build()
                 .toUri();
         var request = new RequestEntity<>(address, HttpMethod.POST, uri);
+
         return template.exchange(request, AddressDto.class).getBody();
     }
 
     @Override
     public void delete(Long id) {
-        var app = eurekaClient.getApplication(serviceName);
-        var instance = app.getInstances().stream().findAny().get();
+        InstanceInfo instance = eurekaClient.getApplication(serviceName)
+                .getInstances()
+                .stream()
+                .findAny()
+                .orElseThrow();
         var uri = UriComponentsBuilder.fromPath(baseUrl + "/{id}")
                 .scheme("http")
                 .host(instance.getHostName())
                 .port(instance.getPort())
                 .buildAndExpand(id)
                 .toUri();
-//        var request = new RequestEntity<>(null, HttpMethod.DELETE, uri);
         template.delete(uri);
     }
 }
