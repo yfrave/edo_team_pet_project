f30cea4026d111b2d78a6042a88c2cac4a0e8d8d
Сергей_Иваненко <kobrelislegryi@gmail.com>
2023-01-25 14:07:58 +0300

    added javadoc comments

--- edo-repository/src/main/java/com/education/util/DtoConverter.java
+++ edo-repository/src/main/java/com/education/util/DtoConverter.java
@@ -4,7 +4,16 @@ package com.education.util;
 import com.education.entity.Address;
 import com.education.model.dto.AddressDto;
 
+/**
+ * Converter Dto for Address
+ */
 public class DtoConverter {
+
+    /**
+     * Convert Address to AddressDto
+     * @param address Address
+     * @return AddressDto
+     */
     public static AddressDto convertTo(Address address) {
         return new AddressDto(address.getId(),
                 address.getFullAddress(),
@@ -19,6 +28,11 @@ public class DtoConverter {
                 address.getFlat());
     }
 
+    /**
+     * Convert AddressDto to Address
+     * @param dto AddressDto
+     * @return Address
+     */
     public static Address convertFrom(AddressDto dto) {
         return new Address(dto.getFullAddress(),
                 dto.getStreet(),
