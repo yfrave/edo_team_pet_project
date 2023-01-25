5868460a71371f9a2ad61f36933f1bfee95d2f16
Сергей_Иваненко <kobrelislegryi@gmail.com>
2023-01-25 13:43:04 +0300

    added Swager annotations

--- edo-repository/src/main/java/com/education/controller/AddressRestController.java
+++ edo-repository/src/main/java/com/education/controller/AddressRestController.java
@@ -7,10 +7,7 @@ import com.education.service.address.AddressService;
 import static com.education.util.DtoConverter.convertTo;
 
 import com.education.util.DtoConverter;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
@@ -25,7 +22,9 @@ import java.util.stream.Collectors;
 @RequestMapping("/api/repository/address")
 @AllArgsConstructor
 @Log
+@ApiModel("Controller for Address")
 public class AddressRestController {
+    @ApiModelProperty("service")
     private AddressService addressService;
 
     @ApiOperation(value = "Получить адрес по d", notes = "Returns an address as per the id")
@@ -50,7 +49,7 @@ public class AddressRestController {
             @ApiResponse(code = 200, message = "Successfully retrieved"),
             @ApiResponse(code = 404, message = "Not found - The addresses was not found")
     })
-    @GetMapping("/findAll")
+    @PostMapping("/findAll")
     public ResponseEntity<List<AddressDto>> fetchAllAddresses(@RequestBody
                                                               @ApiParam(name = "Address list")
                                                               List<Long> idList)
