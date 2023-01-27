afcc7ad538ce5fc0b65c023a773a717eb30f1a30
Антон Латышев <antohahaha91@gmail.com>
2023-01-27 16:50:46 +0300

    Add logging

--- edo-repository/src/main/java/com/education/controller/QuestionController.java
+++ edo-repository/src/main/java/com/education/controller/QuestionController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/repository/question")
+@Log4j2
 public class QuestionController {
     @ApiModelProperty("questionService")
     private final QuestionService questionService;
@@ -23,6 +25,7 @@ public class QuestionController {
     @ApiOperation("Создание обращения")
     @PostMapping("/add")
     public ResponseEntity<QuestionDto> save(@RequestBody QuestionDto question) {
+        log.info("Creating new Question");
         questionService.save(question);
         return question != null
                 ? new ResponseEntity<>(HttpStatus.CREATED)
@@ -32,6 +35,7 @@ public class QuestionController {
     @ApiOperation("Перемещение обращения в архив")
     @PutMapping("/{id}")
     public ResponseEntity<QuestionDto> moveToArchive(@PathVariable("id") Long id) {
+        log.info("Moving Question to archive");
         questionService.moveToArchive(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -39,6 +43,7 @@ public class QuestionController {
     @ApiOperation("Поиск обращения по id")
     @GetMapping("/{id}")
     public ResponseEntity<QuestionDto> findById(@PathVariable("id") Long id) {
+        log.info("Finding Question by id");
         QuestionDto questionDto = questionService.findById(id);
         return questionDto != null
                 ? new ResponseEntity<>(questionDto, HttpStatus.OK)
@@ -48,6 +53,7 @@ public class QuestionController {
     @ApiOperation("Поиск списка обращений по их id")
     @PostMapping("/all")
     public ResponseEntity<List<QuestionDto>> findAllById(@RequestBody List<Long> ids) {
+        log.info("Finding List<Question> by IDs");
         List<QuestionDto> questionsDto = questionService.findAllById(ids);
         return questionsDto != null && !questionsDto.isEmpty()
                 ? new ResponseEntity<>(questionsDto, HttpStatus.OK)
@@ -57,6 +63,7 @@ public class QuestionController {
     @ApiOperation("Поиск неархивированного обращения по id")
     @GetMapping("/notArchived/{id}")
     public ResponseEntity<QuestionDto> findByIdNotArchived(@PathVariable("id") Long id) {
+        log.info("Finding not archived Question by ID");
         QuestionDto questionDto = questionService.findByIdNotArchived(id);
         return questionDto != null
                 ? new ResponseEntity<>(questionDto, HttpStatus.OK)
@@ -66,6 +73,7 @@ public class QuestionController {
     @ApiOperation("Поиск списка неархивированных обращений по их id")
     @PostMapping("/notArchivedAll")
     public ResponseEntity<List<QuestionDto>> findAllByIdNotArchived(@RequestBody List<Long> ids) {
+        log.info("Finding not archived List<Question> by IDs");
         List<QuestionDto> questions = questionService.findAllByIdNotArchived(ids);
         return questions != null && !questions.isEmpty()
                 ? new ResponseEntity<>(questions, HttpStatus.OK)
