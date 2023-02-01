07387eb48746fe84b9275caef06650a33a47c6a2
Сергей_Иваненко <kobrelislegryi@gmail.com>
2023-02-01 17:46:41 +0300

    mirco fix

---
+++ edo-repository/src/main/java/com/education/repository/AddressRepository.java
@@ -0,0 +1,7 @@
+package com.education.repository;
+
+import com.education.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long>{
+}
