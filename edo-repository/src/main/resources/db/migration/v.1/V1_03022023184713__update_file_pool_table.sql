7b841df40d222c1b55e882744e24b84bb12ed121
Рамазан Гаджиев <mm.gadzhiev@mail.ru>
2023-02-05 23:37:45 +0300

    deleted update migration for file_pool table

--- edo-repository/src/main/resources/db/migration/v.1/V1_03022023184713__update_file_pool_table.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE IF EXISTS file_pool
-    DROP COLUMN creator_id,
-    ADD COLUMN IF NOT EXISTS creator_id bigint references employee (id); --Автор файла
-
-
-comment on column file_pool.creator_id is 'Автор файла';
