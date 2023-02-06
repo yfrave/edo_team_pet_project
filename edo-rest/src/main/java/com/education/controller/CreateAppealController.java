a37b2603bdcfb30dc6af109221122431daeb4a7f
Alexey Sementkovskiy <sementkovsky@yandex.ru>
2023-02-06 12:24:28 +0300

    createNewAppeal method added

--- edo-rest/src/main/java/com/education/controller/CreateAppealController.java
+++ edo-rest/src/main/java/com/education/controller/CreateAppealController.java
@@ -5,6 +5,8 @@ import com.education.service.CreatingAppealService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,34 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("api/rest/create_appeal")
 public class CreateAppealController {
+
     private final CreatingAppealService service;
 
-    @PostMapping
-    private void createAppealService(@RequestBody AppealWithRelationsDto json) {
-        service.createAppeal(json);
-    }
-
     @PostMapping("/")
-    public void theMethod(@RequestBody String json) throws JsonProcessingException {
-
+    public ResponseEntity<AppealWithRelationsDto> createNewAppeal(@RequestBody String json) throws JsonProcessingException {
 
         ObjectMapper objectMapper = new ObjectMapper();
-
-// Deserialization into the `Employee` class
         AppealWithRelationsDto appealDto = objectMapper.readValue(json, AppealWithRelationsDto.class);
-        appealDto.getSendingMethod();
-
-
-
-//        UserStory:
-//        Пользователь заполняет Подписанта, Адресатов, выбирает номенклатуру,
-//        заполняет данные автора обращения, заполняет аннотацию, создает новый вопрос(ы),
-//        которые поднял автор: выбирает тему вопроса, заполняет краткое содержание,
-//        загружает файл, нажимает кнопку сохранить и все его данные успешно сохраняются в бд
-
-        System.out.println(appealDto.getSendingMethod());
+        return new ResponseEntity<>(service.createAppeal(appealDto), HttpStatus.CREATED);
     }
-
 }
-
-
