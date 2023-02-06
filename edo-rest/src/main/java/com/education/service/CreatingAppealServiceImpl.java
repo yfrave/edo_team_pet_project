9ffa8b2c84b536b68d642c4c5588764fd877b3fa
Alexey Sementkovskiy <sementkovsky@yandex.ru>
2023-02-06 15:15:42 +0300

    Service methods ander construction

--- edo-rest/src/main/java/com/education/service/CreatingAppealServiceImpl.java
+++ edo-rest/src/main/java/com/education/service/CreatingAppealServiceImpl.java
@@ -1,14 +1,27 @@
 package com.education.service;
 
+import com.education.client.CreateAppealRestTemplateClient;
 import com.education.model.dto.AppealWithRelationsDto;
+import com.education.model.enumEntity.EnumAppealStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CreatingAppealServiceImpl implements CreatingAppealService {
+
+    private final CreateAppealRestTemplateClient client;
 
     @Override
     public AppealWithRelationsDto createAppeal(AppealWithRelationsDto appealDto) {
-        return appealDto;
+
+        client.saveAuthor(appealDto.getAuthors());
+        client.saveQuestion(appealDto.getQuestions());
+        client.saveFilePool(appealDto.getFile());
+        appealDto.setAppealStatus(EnumAppealStatus.NEW);
+        client.saveAppeal(appealDto);
+
+        return client.getAppeal();
 
     }
 }
