ea8cd219ff66ecee72780c681015220b3dc11e8d
Alik Karibov <alik_karibov@mail.ru>
2023-02-08 20:00:03 +0300

    fix appeal migr and entity creationDate archivedDate

--- edo-repository/src/main/resources/db/migration/v.1/V1_130120231536446__create_appeal_table.sql
+++ edo-repository/src/main/resources/db/migration/v.1/V1_130120231536446__create_appeal_table.sql
@@ -1,8 +1,8 @@
 create table if not exists appeal
 (
     id            bigserial    not null primary key,             -- id
-    creation_date timestamptz  not null,                         -- Дата создания обращения
-    archived_date timestamptz,                                   -- Дата архивирования обращения
+    creationDate timestamptz  not null,                         -- Дата создания обращения
+    archivedDate timestamptz,                                   -- Дата архивирования обращения
     number        varchar(255) not null,                         -- Номер обращения
     annotation    varchar(255) not null,                         -- Описание обращения
     creator_id    bigint       not null references employee (id) -- Автор
@@ -26,8 +26,8 @@ comment on column appeal_addressee.appeal_id is 'id appeal';
 comment on column appeal_addressee.employee_id is 'id employee';
 
 comment on column appeal.id is 'id';
-comment on column appeal.creation_date is 'Дата создания обращения';
-comment on column appeal.archived_date is 'Дата архивирования обращения';
+comment on column appeal.creationDate is 'Дата создания обращения';
+comment on column appeal.archivedDate is 'Дата архивирования обращения';
 comment on column appeal.number is 'Номер обращения';
 comment on column appeal.annotation is 'Описание обращения';
 comment on column appeal.creator_id is 'Автор';
