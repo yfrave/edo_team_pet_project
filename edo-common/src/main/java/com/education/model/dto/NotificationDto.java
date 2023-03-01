94cef8750659e6c70b4562b839ce2d57fdc9f44e
Хафизов Ильмир <ilmir131313@yandex.ru>
2023-03-01 10:59:43 +0300

    Исправил NotificationDto

--- edo-common/src/main/java/com/education/model/dto/NotificationDto.java
+++ edo-common/src/main/java/com/education/model/dto/NotificationDto.java
@@ -1,11 +1,14 @@
 package com.education.model.dto;
 
+import com.education.model.enumEntity.EnumNotification;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Set;
 
 @ApiModel("Класс NotificationDto, dto для класса Notification.class")
 @NoArgsConstructor
@@ -16,7 +19,8 @@ public class NotificationDto {
 
     @ApiModelProperty("Id")
     private Long id;
-
+    @ApiModelProperty("Тип оповещения")
+    private EnumNotification enumNotification;
     @ApiModelProperty("Оповещение пользователя")
-    private EmployeeDto employeeNotice;
+    private Set<EmployeeDto> employeeNotice;
 }
