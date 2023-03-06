2efac40d981dea4a0cfa9d2dd74f36b001b58fb7
Luki Daria <one_snowly@mail.ru>
2023-03-07 01:04:20 +0300

    Changed method for uploading files with MultipartFile

--- edo-rest/src/main/java/com/education/service/file/FileService.java
+++ edo-rest/src/main/java/com/education/service/file/FileService.java
@@ -6,5 +6,5 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
 
-    void uploadFile(MultipartFile multipartFile);
+    String uploadFile(MultipartFile multipartFile);
 }
